d13bbf6b175af2a25729e37ed753d2f44adb5fb3
Dream <dream-song@qq.com>
2025-03-28 23:47:11 +0800

    Initialize the warehouse and create a Package

---
+++ Package@swift-5.10.swift
@@ -0,0 +1,25 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Blink",
+    platforms: [.macOS(.v14), .iOS(.v17), .tvOS(.v17), .watchOS(.v10)],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "Blink",
+            targets: ["Blink"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "Blink", path: "Sources"),
+        .testTarget(
+            name: "BlinkTests",
+            dependencies: ["Blink"]
+        ),
+    ]
+)
---
+++ Sources/Blink/Blink+Version.swift
@@ -0,0 +1,12 @@
+//
+//  Blink+Version.swift
+//  Blink
+//
+//  Created by Dream on 2025/3/28.
+//
+
+enum BlinkInfo {
+    
+    /// Current Blink version.
+    public static let version = "1.0.0"
+}
